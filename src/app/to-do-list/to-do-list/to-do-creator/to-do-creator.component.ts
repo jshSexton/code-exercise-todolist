d145831bf14f654e5e8b0d3e2a45124dab3c10d0
jshSexton <jsh.sexton@gmail.com>
2018-08-09 22:41:55 -0700

    Basic file structure

---
+++ src/app/to-do-list/to-do-list/to-do-creator/to-do-creator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-creator',
+  templateUrl: './to-do-creator.component.html',
+  styleUrls: ['./to-do-creator.component.css']
+})
+export class ToDoCreatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
