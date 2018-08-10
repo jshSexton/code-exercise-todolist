d1c96121d7f5cbc80ace211ef0d529dc8aa90641
jshSexton <jsh.sexton@gmail.com>
2018-08-09 22:40:18 -0700

    Basic file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { ToDoListModule } from './to-do-list/to-do-list.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    ToDoListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
