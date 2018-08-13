5a314d97e62af7a21127a987b7a8d7cc0f4abb1c
jshSexton <jsh.sexton@gmail.com>
2018-08-12 22:05:53 -0700

    CSS changes and hovering css changes

--- src/app/to-do-list/to-do-list/to-do-card/to-do-card.component.ts
+++ src/app/to-do-list/to-do-list/to-do-card/to-do-card.component.ts
@@ -1,14 +1,23 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {ItodoItem} from '../to-do-list';
 
+enum HoverState {
+  None = 0,
+  OverCheckbox = 1,
+  OverDelete = 2
+}
+
 @Component({
   selector: 'app-to-do-card',
   templateUrl: './to-do-card.component.html',
   styleUrls: ['./to-do-card.component.scss']
 })
 export class ToDoCardComponent implements OnInit {
-  @Input() todoInfo: ItodoItem;
-  @Output() deleteNotify: EventEmitter<ItodoItem> = new EventEmitter<ItodoItem>();
+  @Input() todoInfo: ItodoItem;   // Get the info from parent
+  @Output() deleteNotify: EventEmitter<ItodoItem> = new EventEmitter<ItodoItem>();    // event to inform parent to remove this item
+
+  currentHoverState: HoverState = HoverState.None;    // controls color and display of the delete button
+  hoverStateEnum = HoverState;    // exposes enum to angular http
 
   constructor() { }
 
@@ -17,5 +26,25 @@ export class ToDoCardComponent implements OnInit {
 
   onDeleteClick() {
     this.deleteNotify.emit(this.todoInfo);
+  }
+
+  // Mouse enters card box
+  onMouseEnter() {
+    this.currentHoverState = HoverState.OverCheckbox;
+  }
+
+  // Mouse leaves card box
+  onMouseLeave() {
+    this.currentHoverState = HoverState.None;
+  }
+
+  // Mouse is over the delete button in particular
+  onMouseOverDelete() {
+    this.currentHoverState = HoverState.OverDelete;
+  }
+
+  // Mouse is no longer over the delete button in particular
+  onMouseOutDelete() {
+    this.currentHoverState = HoverState.OverCheckbox;
   }
 }
